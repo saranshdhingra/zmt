9eed5b6931feb46c5242b20f44dec4d44666d174
supernova13892 <saranshdhingra92@gmail.com>
2018-12-22 20:11:31 +0530

    Starting to add an environment file

---
+++ js/environment.js
@@ -0,0 +1,13 @@
+const env={
+	baseUrl : "https://zohomailtracker.com/api/v2/",
+	publicVapidKey: "",
+	firebaseConfig: {
+		apiKey: "",
+		authDomain: "",
+		databaseURL: "",
+		projectId: "",
+		storageBucket: "",
+		messagingSenderId: ""
+	}
+
+}
