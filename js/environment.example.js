e30d36745add2d0d9ac0079a299f4e4cd32c6988
supernova13892 <saranshdhingra92@gmail.com>
2020-10-09 18:20:51 +0530

    Cleanup environment.example files

--- js/environment.example.js
+++ js/environment.example.js
@@ -1,9 +1,8 @@
 const env = {
-	baseUrl: "https://zohomailtracker.com/api/v3/",
-	webLoginUrl: "https://zohomailtracker.com/web-login",
-	publicVapidKey: "",
+	baseUrl: 'https://zohomailtracker.com/api/v3/',
+	webLoginUrl: 'https://zohomailtracker.com/web-login',
 	pubnub: {
 		subscribeKey: ''
 	}
 
-}
+};
