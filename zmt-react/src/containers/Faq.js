c94b4fa39a81c934d9e5f848f1ef6d44243a5818
supernova13892 <saranshdhingra92@gmail.com>
2020-10-09 17:39:09 +0530

    Updated FAQ a bit

--- zmt-react/src/containers/Faq.js
+++ zmt-react/src/containers/Faq.js
@@ -18,21 +18,33 @@ class Faq extends React.Component {
             {
                 'title': 'How is the tracking accomplished?',
                 'text': `<p>This is actually a common technique used by most tracking software. 
-                            This is used by all big players like <strong>Mailchimp</strong>, <strong>Mailgun</strong>, and any other provide that gives you the tracking feature.
-                            Even <strong>Zoho</strong> uses this in their campaign emails.</p>
+                            This is used by all big players like <code>Mailchimp</code>, <code>Mailgun</code>, and any other provide that gives you the tracking feature.
+                            Even <code>Zoho</code> uses this in their campaign emails.</p>
                          <p>We simply insert an image inside your mail which when loaded, tells that the email has been read.</p>`
             },
             {
                 'title': 'Sometimes a user has viewed an email but I can\'t see that in the list!',
-                'text': `<p>As explained above, we insert an 1x1 px image inside your email.
+                'text': `<p>As explained above, we insert an <code>1x1 px</code> image inside your email.
                             To be able to record if the email has been opened, that image will have to be loaded. 
-                            So, a common scenario is if the recipient has turned off images for your emails then we won\'t be able to record it!</p>`
+                            So, a common scenario is if the recipient has turned off images for your emails then we won't be able to record it!</p>`
             },
             {
                 'title': 'How accurately do you display the IP address, location etc?',
                 'text': `<p>The techniques used to get this info is pretty standard.
                         If a person is behind a proxy, the IP address will obviously be wrong.
-                        Similarly, there are limitations to get location, device info etc. Whatever info I can record, I am displaying to you!</p>`
+                        Similarly, there are limitations to get location, device info etc. Whatever info we can record, we are displaying to you!</p>`
+            },
+            {
+                'title': 'Sometimes I get absolutely incorrect IP/location, why is that?',
+                'text': `<p>Many email clients don't show images directly from our server(<em>for security reasons of course</em>).
+                        Previously, <code>Gmail</code> used to do it, but <code>Zoho</code> also started the same some time back.
+                        So, sometimes the images are loaded from their servers and served to your recipients, hence the location isn't the same as your recipients.</p>`
+            },
+            {
+                'title': 'How do you handle replies?',
+                'text': `<p>When you are in a reply thread or forwarding your emails, to prevent getting multiple views on a single mail, 
+                            we remove the old trackers before inserting new ones.
+                            <u>But the behaviour highly depends on the message being replied to and how the email clients sends the images in replies</u>.</p>`
             },
             {
                 'title': 'Are you recording my email contents?',
@@ -43,7 +55,8 @@ class Faq extends React.Component {
             {
                 'title': 'If I open my own email, I don\'t get a notification, why?',
                 'text': `<p>If you are testing the functionality of the extension, I recommend you to open your emails in another browser, because we try not to count your own opening as a <em>'view'</em>.
-                            If we were to record your own opens, it will be highly inaccurate!</p>`
+                            If we were to record your own opens, it will be highly inaccurate!
+                            If you think that is happening, do file a Bug Report on this please :)</p>`
             },
             {
                 'title': 'Sometimes, I can\'t send an email because of your extension!',
@@ -52,7 +65,7 @@ class Faq extends React.Component {
             },
             {
                 'title': 'What about Android, iOS apps?',
-                'text': `<p>There is an Android app, but its being revamped.
+                'text': `<p>There is an Android app, but it's being revamped.
                             So far, we have not started work on an iOS app!</p>`
             }
         ];
